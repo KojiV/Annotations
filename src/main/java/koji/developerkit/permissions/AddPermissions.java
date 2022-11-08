7efca3a51d7e0c699f562a5b52fc4b64554cd3b1
G-K-G <69867605+G-K-G@users.noreply.github.com>
2022-11-08 14:24:20 -0500

    testing with new annotation

---
+++ src/main/java/koji/developerkit/permissions/AddPermissions.java
@@ -0,0 +1,16 @@
+package koji.developerkit.permissions;
+
+import org.bukkit.permissions.PermissionDefault;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface AddPermissions {
+    PermissionDefault permission() default PermissionDefault.OP;
+    String description() default "";
+    String prefix();
+}
---
+++ src/main/java/koji/developerkit/permissions/PermissionAnnotationProcessor.java
@@ -0,0 +1,159 @@
+package koji.developerkit.permissions;
+
+import com.google.auto.service.AutoService;
+import com.google.common.collect.Maps;
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfoList;
+import org.bukkit.permissions.PermissionDefault;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.nodes.Tag;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+import java.util.Set;
+
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedAnnotationTypes("koji.developerkit.permissions.AddPermissions")
+@AutoService(Processor.class)
+public class PermissionAnnotationProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnvironment) {
+        try {
+            FileObject file = processingEnv.getFiler().getResource(
+                    StandardLocation.CLASS_OUTPUT, "", "plugin.yml"
+            );
+
+            annotations.forEach(a -> processAnnotation(a, roundEnvironment, file));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    private void processAnnotation(TypeElement annotation, RoundEnvironment roundEnvironment, FileObject file) {
+        roundEnvironment.getElementsAnnotatedWith(annotation).forEach(a -> {
+            AnnotationMirror mirror = a.getAnnotationMirrors().stream().filter(b ->
+                    b.getAnnotationType().toString().equals(
+                            AddPermissions.class.getName()
+                    )
+            ).findAny().get();
+            Map<? extends ExecutableElement, ? extends AnnotationValue> elementValuesWithDefaults =
+                    processingEnv.getElementUtils().getElementValuesWithDefaults(mirror);
+
+            AnnotationValue permissionLevelValue = annotationValue("permission", elementValuesWithDefaults);
+            AnnotationValue prefixValue = annotationValue("prefix", elementValuesWithDefaults);
+            AnnotationValue descriptionValue = annotationValue("description", elementValuesWithDefaults);
+
+            String prefix = (String) prefixValue.getValue();
+            PermissionDefault permissionLevel = (PermissionDefault) permissionLevelValue.getValue();
+            String description = (String) descriptionValue.getValue();
+
+            ClassInfoList enchants = new ClassGraph()
+                    .enableClassInfo()
+                    .enableAnnotationInfo()
+                    .scan()
+                    .getClassInfo(a.asType().toString())
+                    .getSubclasses();
+
+            Map<String, Object> yml = Maps.newLinkedHashMap();
+            Map<String, Map<String, Object>> permissionMetadata = Maps.newLinkedHashMap();
+            enchants.forEach(b ->
+                permissionMetadata.put(
+                        prefix + "." + b.getSimpleName().toLowerCase(),
+                        processPermission(
+                                description,
+                                permissionLevel
+                        )
+                )
+            );
+            yml.put("permissions", permissionMetadata);
+            try {
+                Yaml yaml = new Yaml();
+                try (Writer w = file.openWriter()){
+                    String raw = yaml.dumpAs(yml, Tag.MAP, DumperOptions.FlowStyle.BLOCK);
+                    w.write(raw);
+                    w.flush();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    protected Map<String, Object> processPermission(String description, PermissionDefault permissionDefault) {
+        Map<String, Object> permission = Maps.newLinkedHashMap();
+
+        if (!"".equals(description)) {
+            permission.put("description", description);
+        }
+
+        if (PermissionDefault.OP != permissionDefault) {
+            permission.put("default", permissionDefault.toString().toLowerCase());
+        }
+
+        return permission;
+    }
+
+    private AnnotationValue annotationValue(
+            String value,
+            Map<? extends ExecutableElement, ? extends AnnotationValue> elementValuesWithDefaults
+    ) {
+        return elementValuesWithDefaults.keySet()
+                .stream()
+                .filter(b -> b.getSimpleName().toString().equals(value))
+                .map(elementValuesWithDefaults::get).findAny().get();
+    }
+
+    public static String capitalize(String str) {
+        return capitalize(str, null);
+    }
+
+    public static String capitalize(String str, char[] delimiters) {
+        int delimLen = (delimiters == null ? -1 : delimiters.length);
+        if (str == null || str.length() == 0 || delimLen == 0) {
+            return str;
+        }
+        int strLen = str.length();
+        StringBuilder buffer = new StringBuilder(strLen);
+        boolean capitalizeNext = true;
+        for (int i = 0; i < strLen; i++) {
+            char ch = str.charAt(i);
+
+            if (isDelimiter(ch, delimiters)) {
+                buffer.append(ch);
+                capitalizeNext = true;
+            } else if (capitalizeNext) {
+                buffer.append(Character.toTitleCase(ch));
+                capitalizeNext = false;
+            } else {
+                buffer.append(ch);
+            }
+        }
+        return buffer.toString();
+    }
+
+    private static boolean isDelimiter(char ch, char[] delimiters) {
+        if (delimiters == null) {
+            return Character.isWhitespace(ch);
+        }
+        for (char delimiter : delimiters) {
+            if (ch == delimiter) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
